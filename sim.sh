075c567c3ca8d7a7699005c2adf2406b6e094798
Vard Lott <vlott@ldschurch.org>
2009-12-02 22:47:26 +0000

    new scripts - very basic - will need to be modified to fit packaging

---
+++ sim.sh
@@ -0,0 +1,8 @@
+# User needs to set M2_REPO and JAVA_HOME and have $JAVA_HOME/bin in their PATH
+#
+# export M2_REPO='/Users/<USER>/.m2/repository'
+#
+#
+export CONFIG_FILE="$M2_REPO/org/lds/sso/login-simulator/1.0-SNAPSHOT/community_logins.xml"
+echo $M2_REPO/org/lds/sso/login-simulator/1.0-SNAPSHOT/login-simulator-1.0-SNAPSHOT.jar:$M2_REPO/org/mortbay/jetty/jetty/6.1.7/jetty-6.1.7.jar:$M2_REPO/org/mortbay/jetty/jetty-util/6.1.7/jetty-util-6.1.7.jar:$M2_REPO/log4j/log4j/1.2.12/log4j-1.2.12.jar:$M2_REPO/org/mortbay/jetty/servlet-api-2.5/6.1.7/servlet-api-2.5-6.1.7.jar:$M2_REPO/org/mortbay/jetty/jsp-2.1/6.1.7/jsp-2.1-6.1.7.jar:$M2_REPO/org/mortbay/jetty/jsp-api-2.1/6.1.7/jsp-api-2.1-6.1.7.jar:$M2_REPO/ant/ant/1.6.5/ant-1.6.5.jar:$JAVA_HOME/lib/tools.jar org.lds.sso.appwrap.Service $CONFIG_FILE
+java -cp $M2_REPO/org/lds/sso/login-simulator/1.0-SNAPSHOT/login-simulator-1.0-SNAPSHOT.jar:$M2_REPO/org/mortbay/jetty/jetty/6.1.7/jetty-6.1.7.jar:$M2_REPO/org/mortbay/jetty/jetty-util/6.1.7/jetty-util-6.1.7.jar:$M2_REPO/log4j/log4j/1.2.12/log4j-1.2.12.jar:$M2_REPO/org/mortbay/jetty/servlet-api-2.5/6.1.7/servlet-api-2.5-6.1.7.jar:$M2_REPO/org/mortbay/jetty/jsp-2.1/6.1.7/jsp-2.1-6.1.7.jar:$M2_REPO/org/mortbay/jetty/jsp-api-2.1/6.1.7/jsp-api-2.1-6.1.7.jar:$M2_REPO/ant/ant/1.6.5/ant-1.6.5.jar:$JAVA_HOME/lib/tools.jar org.lds.sso.appwrap.Service $CONFIG_FILE
