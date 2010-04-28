69c16bdf187624a66a5fe304def4d99e4a8a105d
mccurdydl <>
2010-04-28 19:31:16 +0000

    Adding for continuous build.

---
+++ mvn.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export JAVA_HOME=/usr/java/jdk1.6.0_14
+export M2_HOME=/usr/maven/maven-2.2.1
+export MAVEN_OPTS=-Xmx512m
+export PATH=/usr/local/bin:/bin:/usr/bin:/usr/X11R6/bin:/home/bldmgr/bin:$M2_HOME/bin:$JAVA_HOME/bin:.
+export DISPLAY=`cat /var/xvfb/DISPLAY` XAUTHORITY=/var/xvfb/XAUTHORITY
+
+svn update
+
+DATE=`date +%m%d%H%M`
+REVISION=`svn info | grep Revision | awk '{ print $2 }'`
+SVNURL=`svn info | grep URL | awk '{ print $2 }'`
+
+echo "Building and gathering test results, please wait..."
+
+mvn clean install "$@" -B \
+                -DbuildDate=$DATE \
+                -DbuildRevision=$REVISION \
+                -DbuildUrl=$SVNURL
+
