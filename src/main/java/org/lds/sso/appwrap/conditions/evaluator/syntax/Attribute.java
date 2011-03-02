6aaf7cf3fbcd2b22f077cedf784f32cb3198c9be
gmdayley <>
2011-03-02 22:00:57 +0000

    added support for Attribute

--- src/main/java/org/lds/sso/appwrap/conditions/evaluator/syntax/Attribute.java
+++ src/main/java/org/lds/sso/appwrap/conditions/evaluator/syntax/Attribute.java
@@ -15,34 +15,132 @@ import java.util.Map;
  * To change this template use File | Settings | File Templates.
  */
 public class Attribute extends SyntaxBase {
+    private static final String EXTENSIBLE_CASE_EXACT_SUBSTRING_MATCH = "caseExactSubstringMatch";
+    private static final String EXTENSIBLE_CASE_EXACT_MATCH = "caseExactMatch";
+    private static final String CONFIG_NAME = "name";
+    private static final String CONFIG_OPERATION = "operation";
+    private static final String CONFIG_VALUE = "value";
+
     private Attribute.Operation operation;
     private String attributeName;
     private String operationName;
     private String attributeValue;
+    private boolean caseExactSubstringMatch;
+    private boolean caseExactMatch;
 
     @Override
     public void init(String syntax, Map<String, String> cfg) throws EvaluationException {
         super.init(syntax, cfg);
-        this.attributeName = cfg.get("name");
-        this.operationName = cfg.get("operation");
-        this.attributeValue = cfg.get("value");
-        this.operation = Operation.getOperation(cfg.get("operation"));
+        this.attributeName = super.getRequired(CONFIG_NAME, cfg);//parseAttributeName(cfg.get(CONFIG_NAME));
+        this.operationName = super.getRequired(CONFIG_OPERATION, cfg);
+        this.operation = Operation.getOperation(cfg.get(CONFIG_OPERATION));
+        if(operation == Attribute.Operation.EQUALS){
+            this.attributeValue = super.getRequired(CONFIG_VALUE, cfg);
+        }
     }
 
-    @Override
-    public boolean isConditionSatisfied(EvaluationContext evaluationcontext) throws EvaluationException {
-        User user = evaluationcontext.user;
+   /* private String parseAttributeName(String name) {
+        if(name.contains(":") && name.endsWith(":")){//Check for Extensible Matching
+            String extensible = name.substring(name.indexOf(":"), name.lastIndexOf(":"));
+            if(EXTENSIBLE_CASE_EXACT_MATCH.equals(extensible)){
+                caseExactMatch = true;
+            }
+            else if(EXTENSIBLE_CASE_EXACT_SUBSTRING_MATCH.equals(extensible)){
+                caseExactSubstringMatch = true;
+            }
+            return name.substring(name.indexOf(":"));
+        }
+        return name;
+    }*/
 
-        switch (operation){
+    @Override
+    public boolean isConditionSatisfied(EvaluationContext evaluationCtx) throws EvaluationException {
+        User user = evaluationCtx.user;
+
+        boolean satisfied;
+        switch (operation) {
             case EQUALS:
-                return user.hasAttributeValue(attributeName, attributeValue);
+                NvPair[] attribute = user.getAttribute(attributeName);
+                satisfied = wildCardMatch(attribute[0].getValue(), attributeValue);
+                if(satisfied){
+                    debug(evaluationCtx, satisfied, String.format("user has attribute that matches value, actual: %s", attribute[0].getValue()));
+                }
+                else{
+                    debug(evaluationCtx, satisfied, String.format("user does not have attribute that matches value, actual: %s", attribute[0].getValue()));
+                }
+
+                break;
             case EXISTS:
-//                return user.hasAttribute(attributeName);
-                return false;
+                satisfied = user.hasAttribute(attributeName);
+                debug(evaluationCtx, satisfied, String.format("Evaluating if user has attribute[%s] %b", attributeName, satisfied));
+                break;
             case NOT_SUPPPORTED:
                 throw new EvaluationException("Operation not supported: " + operationName);
             default:
                 return false;
+        }
+
+        return satisfied;
+    }
+
+    public static boolean wildCardMatch(String text, String pattern){
+        boolean startWithWildcard = pattern.startsWith("*");
+        boolean endsWithWildcard = pattern.endsWith("*");
+        String [] tokens = pattern.split("(?<!\\\\)\\*"); //Find all * that don't have a \ before them
+
+        //Escape all * in text string to match escaped * in pattern
+        text = text.replaceAll("\\*", "\\\\*");
+        //TODO: case sensitivity
+
+
+        for (int i = 0, tokensLength = tokens.length; i < tokensLength; i++) {
+            String card = tokens[i];
+
+            //todo: possibly move these outside the loop for performance...
+            if(i == 0){//First time
+                if(!startWithWildcard){
+                    if(!text.startsWith(card)){
+                        return false;
+                    }
+                }
+            }
+            else if(i == tokensLength-1){//Last time
+                if(!endsWithWildcard){
+                    if(!text.endsWith(card)){
+                        return false;
+                    }
+                }
+            }
+
+            int idx = text.indexOf(card);
+            if (idx == -1) { return false;}
+            text = text.substring(idx + card.length());
+        }
+
+        return true;
+    }
+
+/*    public static boolean wildCardMatch2(String text, String pattern) {
+//        int idx = pattern.indexOf("(?<!\\\\)\\*");
+        String[] cards = pattern.split("\\*", 2);
+        //Find card
+
+        int idx = text.indexOf(cards[0]);
+        if (idx == -1) {return false;}
+        text = text.substring(idx + cards[0].length());
+
+
+        if (cards.length == 1) { // No wildcard
+            return cards[0].equals("") || text.equals("");
+        }
+        else {
+            return wildCardMatch2(text, cards[1]);
+        }
+    }*/
+
+    public void debug(EvaluationContext ctx, boolean outcome, String message){
+        if(ctx.shouldLogResult(this)){
+            ctx.logResult(this, outcome, message);
         }
     }
 
@@ -51,16 +149,16 @@ public class Attribute extends SyntaxBase {
         EQUALS,
         NOT_SUPPPORTED;
 
-        public static Operation getOperation(String operation){
-            if("exists".equalsIgnoreCase(operation)){
+        public static Operation getOperation(String operation) {
+            if ("exists".equalsIgnoreCase(operation)) {
                 return EXISTS;
-            }
-            else if("equals".equalsIgnoreCase(operation)){
+            } else if ("equals".equalsIgnoreCase(operation)) {
                 return EQUALS;
-            }
-            else{
+            } else {
                 return NOT_SUPPPORTED;
             }
         }
     }
+
+
 }
--- src/test/java/org/lds/sso/appwrap/conditions/evaluator/syntax/AttributeTest.java
+++ src/test/java/org/lds/sso/appwrap/conditions/evaluator/syntax/AttributeTest.java
@@ -1,12 +1,16 @@
 package org.lds.sso.appwrap.conditions.evaluator.syntax;
 
 import org.easymock.classextension.EasyMock;
+import org.lds.sso.appwrap.NvPair;
 import org.lds.sso.appwrap.User;
 import org.lds.sso.appwrap.conditions.evaluator.EvaluationContext;
+import org.lds.sso.appwrap.conditions.evaluator.EvaluationException;
 import org.lds.sso.appwrap.conditions.evaluator.IEvaluator;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
+import java.io.StringReader;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -24,7 +28,7 @@ public class AttributeTest extends TestBaseClass {
 		IEvaluator ev = eng.getEvaluator("test-evaluator", "<Attribute name='test' operation='equals' value='test'/>");
 
         User usr = EasyMock.createMock(User.class);
-        EasyMock.expect(usr.hasAttributeValue("test", "test")).andReturn(true);
+        EasyMock.expect(usr.getAttribute("test")).andReturn(new NvPair[]{new NvPair("test", "test")});
         EasyMock.replay(usr);
 
         Map<String,String> env = new TreeMap<String,String>();
@@ -34,13 +38,103 @@ public class AttributeTest extends TestBaseClass {
 
 	}
 
+    @Test
+	public void testAttributeOperationEqualsWithWildcardTrue() throws Exception {
+		IEvaluator ev = eng.getEvaluator("test-evaluator", "<Attribute name='test' operation='equals' value='this * test' debug='true'/>");
+
+        User usr = EasyMock.createMock(User.class);
+        EasyMock.expect(usr.getAttribute("test")).andReturn(new NvPair[]{new NvPair("test", "this is a super test")});
+        EasyMock.expect(usr.getUsername()).andReturn("ngienglishbishop");
+        EasyMock.replay(usr);
+
+        Map<String,String> env = new TreeMap<String,String>();
+
+        EvaluationContext ctx = new EvaluationContext(usr, env);
+		Assert.assertTrue(ev.isConditionSatisfied(ctx), "should have attribute called test with value that matches: this * test");
+
+	}
+
+    @Test
+	public void testAttributeOperationEqualsWithEndingWildcardTrue() throws Exception {
+		IEvaluator ev = eng.getEvaluator("test-evaluator", "<Attribute name='test' operation='equals' value='this * test *' debug='true'/>");
+
+        User usr = EasyMock.createMock(User.class);
+        EasyMock.expect(usr.getAttribute("test")).andReturn(new NvPair[]{new NvPair("test", "this is a super test don't you think")});
+        EasyMock.expect(usr.getUsername()).andReturn("ngienglishbishop");
+        EasyMock.replay(usr);
+
+        Map<String,String> env = new TreeMap<String,String>();
+
+        EvaluationContext ctx = new EvaluationContext(usr, env);
+		Assert.assertTrue(ev.isConditionSatisfied(ctx), "should have attribute called test with value that matches: this * test *");
+
+	}
+
+    @Test
+	public void testAttributeOperationEqualsNotEndingWildcardFalse() throws Exception {
+		IEvaluator ev = eng.getEvaluator("test-evaluator", "<Attribute name='test' operation='equals' value='this * test'/>");
+
+        User usr = EasyMock.createMock(User.class);
+        EasyMock.expect(usr.getAttribute("test")).andReturn(new NvPair[]{new NvPair("test", "this is a super test don't you think")});
+        EasyMock.expect(usr.getUsername()).andReturn("ngienglishbishop");
+        EasyMock.replay(usr);
+
+        Map<String,String> env = new TreeMap<String,String>();
+
+        EvaluationContext ctx = new EvaluationContext(usr, env);
+		Assert.assertFalse(ev.isConditionSatisfied(ctx), "should not have attribute called test that matches: this * test");
+
+	}
+    @Test
+	public void testAttributeOperationEqualsWithWildcardFalse() throws Exception {
+		IEvaluator ev = eng.getEvaluator("test-evaluator", "<Attribute name='test' operation='equals' value='this is not * test'/>");
+
+        User usr = EasyMock.createMock(User.class);
+        EasyMock.expect(usr.getAttribute("test")).andReturn(new NvPair[]{new NvPair("test", "this is a super test")});
+        EasyMock.replay(usr);
+
+        Map<String,String> env = new TreeMap<String,String>();
+
+        EvaluationContext ctx = new EvaluationContext(usr, env);
+		Assert.assertFalse(ev.isConditionSatisfied(ctx), "should not have attribute called test that matches: this is not * test");
+
+	}
+
+    @Test(expectedExceptions = {EvaluationException.class})
+	public void testAttributeOperationNotSupported() throws Exception {
+		IEvaluator ev = eng.getEvaluator("test-evaluator", "<Attribute name='test' operation='bogus' value='this is not * test'/>");
+
+        User usr = EasyMock.createMock(User.class);
+        EasyMock.expect(usr.getAttribute("test")).andReturn(new NvPair[]{new NvPair("test", "this is a super test")});
+        EasyMock.replay(usr);
+
+        Map<String,String> env = new TreeMap<String,String>();
+
+        EvaluationContext ctx = new EvaluationContext(usr, env);
+		ev.isConditionSatisfied(ctx);
+	}
+
+    @Test
+	public void testAttributeOperationEqualsWithWildcardEscapedWildcard() throws Exception {
+		IEvaluator ev = eng.getEvaluator("test-evaluator", "<Attribute name='test' operation='equals' value='this is not \\* test'/>");
+
+        User usr = EasyMock.createMock(User.class);
+        EasyMock.expect(usr.getAttribute("test")).andReturn(new NvPair[]{new NvPair("test", "this is not * test")});
+        EasyMock.replay(usr);
+
+        Map<String,String> env = new TreeMap<String,String>();
+
+        EvaluationContext ctx = new EvaluationContext(usr, env);
+		Assert.assertTrue(ev.isConditionSatisfied(ctx), "should have attribute called test that matches: this is not \\* test");
+	}
+
 
     @Test
 	public void testAttributeOperationExists() throws Exception {
 		IEvaluator ev = eng.getEvaluator("test-evaluator", "<Attribute name='test' operation='exists' value='test'/>");
 
         User usr = EasyMock.createMock(User.class);
-        EasyMock.expect(usr.hasAttributeValue("test", "test")).andReturn(true);
+        EasyMock.expect(usr.hasAttribute("test")).andReturn(true);
         EasyMock.replay(usr);
 
         Map<String,String> env = new TreeMap<String,String>();
@@ -49,4 +143,38 @@ public class AttributeTest extends TestBaseClass {
 		Assert.assertTrue(ev.isConditionSatisfied(ctx), "should have attribute called test");
 	}
 
+    @Test
+	public void testAttributeOperationEqualsDebug() throws Exception {
+		IEvaluator ev = eng.getEvaluator("test-evaluator", "<Attribute name='test' operation='equals' value='this is not \\* test' debug='true'/>");
+
+        User usr = EasyMock.createMock(User.class);
+        EasyMock.expect(usr.getAttribute("test")).andReturn(new NvPair[]{new NvPair("test", "this is not * test")});
+        EasyMock.replay(usr);
+
+        Map<String,String> env = new TreeMap<String,String>();
+
+        EvaluationContext ctx = new EvaluationContext(usr, env);
+		Assert.assertTrue(ev.isConditionSatisfied(ctx), "should have attribute called test with value test");
+
+        String output = swa.getLogOutput();
+		StringReader sr = new StringReader(output);
+		BufferedReader br = new BufferedReader(sr);
+
+		Assert.assertEquals(br.readLine(), "T  <Attribute debug='true' name='test' operation='equals' value='this is not \\* test'/>  user has attribute that matches value, actual: this is not * test");
+	}
+
+
+    @Test(expectedExceptions = {EvaluationException.class})
+	public void testAttributeValueRequired() throws Exception {
+		IEvaluator ev = eng.getEvaluator("test-evaluator", "<Attribute name='test' operation='equals'/>");
+
+        User usr = EasyMock.createMock(User.class);
+        EasyMock.expect(usr.getAttribute("test")).andReturn(new NvPair[]{new NvPair("test", "this is not * test")});
+        EasyMock.replay(usr);
+
+        Map<String,String> env = new TreeMap<String,String>();
+
+        EvaluationContext ctx = new EvaluationContext(usr, env);
+		ev.isConditionSatisfied(ctx);
+	}
 }
